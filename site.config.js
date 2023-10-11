c5d573db06642585aa08952a1e3cf9841810787f
2wndrhs <2wndrhs@gmail.com>
2023-10-11 22:37:42 +0900

    chore: Notion Page Id 변경

--- site.config.js
+++ site.config.js
@@ -30,7 +30,7 @@ const CONFIG = {
 
   // notion configuration (required)
   notionConfig: {
-    pageId: process.env.NOTION_PAGE_ID,
+    pageId: "6af794ada7a14f1b99daa83531babc99" || process.env.NOTION_PAGE_ID,
   },
 
   // plugin configuration (optional)
